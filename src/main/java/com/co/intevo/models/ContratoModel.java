58242c16cfb89991c2512f81e243839e6ebe0dd9
intevo <sariasj2022@gmail.com>
2023-01-16 10:10:00 -0500

    'Inclusión back variable'

--- src/main/java/com/co/intevo/models/ContratoModel.java
+++ src/main/java/com/co/intevo/models/ContratoModel.java
@@ -11,51 +11,73 @@ public class ContratoModel {
     @Id
     @Column(unique = true, nullable = false, name = "idContrato")
     private long idContrato;
+    private String nombreContrato;
     private String fechaIncioContrato;
     private String fechaFinalizacionContrato;
     private long valorContrato;
+    private int valorNetoContrato;
     private int prorroga;
+    
 
     public ContratoModel() {
     }
 
-    public long getidContrato() {
+    public long getIdContrato() {
         return idContrato;
     }
 
-    public void setidContrato(long idContrato) {
+    public void setIdContrato(long idContrato) {
         this.idContrato = idContrato;
     }
 
-    public String getfechaIncioContrato() {
+    public String getFechaIncioContrato() {
         return fechaIncioContrato;
     }
 
-    public void setfechaIncioContrato(String fechaIncioContrato) {
+    public void setFechaIncioContrato(String fechaIncioContrato) {
         this.fechaIncioContrato = fechaIncioContrato;
     }
 
-    public String getfechaFinalizacionContrato() {
+    public String getFechaFinalizacionContrato() {
         return fechaFinalizacionContrato;
     }
 
-    public void setfechaFinalizacionContrato(String fechaFinalizacionContrato) {
+    public void setFechaFinalizacionContrato(String fechaFinalizacionContrato) {
         this.fechaFinalizacionContrato = fechaFinalizacionContrato;
     }
 
     public long getValorContrato() {
         return valorContrato;
     }
-    
+
     public void setValorContrato(long valorContrato) {
         this.valorContrato = valorContrato;
     }
-    
+
+    public String getNombreContrato() {
+        return nombreContrato;
+    }
+
+    public void setNombreContrato(String nombreContrato) {
+        this.nombreContrato = nombreContrato;
+    }
+
     public int getProrroga() {
         return prorroga;
     }
-    
+
     public void setProrroga(int prorroga) {
         this.prorroga = prorroga;
     }
+
+    public int getValorNetoContrato() {
+        return valorNetoContrato;
+    }
+
+    public void setValorNetoContrato(int valorNetoContrato) {
+        this.valorNetoContrato = valorNetoContrato;
+    }
+
+    
+    
 }
