ca6ca0c1af30a5a6529a62cd8103b6d6875fc9ca
Faber <stiven17arturo@gmail.com>
2023-01-18 11:57:17 -0500

    'Ingreso variable prorroga / crearFacturas'

--- src/main/java/com/co/intevo/models/FacturaModel.java
+++ src/main/java/com/co/intevo/models/FacturaModel.java
@@ -20,6 +20,7 @@ public class FacturaModel {
     private String descripcionServicios;
     private String observacionFactura;
     private Long contrato;
+    private int  prorroga;
 
     public FacturaModel() {
     }
@@ -102,6 +103,14 @@ public class FacturaModel {
 
     public void setContrato(Long contrato) {
         this.contrato = contrato;
+    }
+
+    public int getProrroga() {
+        return this.prorroga;
+    }
+
+    public void setProrroga(int prorroga) {
+        this.prorroga = prorroga;
     }
     
 }
