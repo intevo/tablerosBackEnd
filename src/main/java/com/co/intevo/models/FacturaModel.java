5c4161521105ee8cf2ff4f69b56e5d4a78ee2453
Faber <stiven17arturo@gmail.com>
2023-01-20 11:50:47 -0500

    'prorroga en factura'

--- src/main/java/com/co/intevo/models/FacturaModel.java
+++ src/main/java/com/co/intevo/models/FacturaModel.java
@@ -20,7 +20,6 @@ public class FacturaModel {
     private String descripcionServicios;
     private String observacionFactura;
     private Long contrato;
-    private int  prorroga;
 
     public FacturaModel() {
     }
@@ -108,14 +107,6 @@ public class FacturaModel {
 
     public void setContrato(Long contrato) {
         this.contrato = contrato;
-    }
-
-    public int getProrroga() {
-        return this.prorroga;
-    }
-
-    public void setProrroga(int prorroga) {
-        this.prorroga = prorroga;
     }
     
 }
